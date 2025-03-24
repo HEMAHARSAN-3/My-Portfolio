623c505c4b30c9253703a55ad68f6a15b608c1eb
HEMA HARSAN <hemaharsan3@gmail.com>
2025-03-24 16:55:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,40 +54,6 @@ function openMenu() {
 }
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const text = `
-Hello! I'm Hema Harsan R, a passionate B.Tech student specializing in Artificial Intelligence and Data Science. My journey in technology began at a young age, and I have always been fascinated by the potential of AI to transform industries and improve lives.I actively engage in various projects that allow me to apply my knowledge in real-world scenarios. I have experience working with Python, R, and various data visualization tools, and I am always eager to learn new technologies and methodologies.    `;
-    const typingEffect = document.getElementById("typing-effect");
-    let index = 0;
-
-    function type() {
-        if (index < text.length) {
-            typingEffect.textContent += text[index];
-            index++;
-            setTimeout(type, 50);
-        }
-    }
-
-    type();
-});
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector("nav");
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector("nav ul");
 
-    // Change Navbar Background on Scroll
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 50) {
-            navbar.classList.add("scrolled");
-        } else {
-            navbar.classList.remove("scrolled");
-        }
-    });
-
-    // Toggle Mobile Menu
-    hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-    });
-});
